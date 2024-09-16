d6e0e4039e0e61aadb0943e661461ec2f152d432
Ignacio Dotta <ij.dotta@gmail.com>
2024-09-15 21:05:15 -0300

    clean up

--- kinematic/kinematic_equations.py
+++ kinematic/kinematic_equations.py
@@ -1,20 +1,9 @@
 import numpy as np
 
-def variation(num, den):
-    assert len(num) == len(den)
-    var = np.zeros(len(num))
-    for i in range(len(num)-1):
-        delta_num = num[i+1] - num[i]
-        delta_den = den[i+1] - den[i]
-        if delta_den == 0:
-            continue
-        var[i+1] = delta_num / delta_den
-    return var
-
-def velocity(positions, time):
-    return variation(positions, time)
-
-def acceleration(velocity, time):
-    accel = variation(velocity, time)
-    accel[0:2] = 0
-    return accel
+def variation(x: np.ndarray, y: np.ndarray):
+    assert x.shape == y.shape and len(x) == len(y)
+    variation = [(yf - yi) / (xf - xi) for xi, xf, yi, yf in zip(x[:-1], x[1:], y[:-1], y[1:])]
+    template = np.zeros_like(x)
+    # print(f'Input len: {len(x)} ; Output len: {len(variation)}')
+    template[-len(variation):] = variation
+    return template

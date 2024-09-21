906dd7adcffd1db1103b49c38b9e42880ca3955c
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-09-21 11:55:46 -0300

    Se refactorizo el codigo parar hacerlo mas modular

--- tracker_articulations.py
+++ tracker_articulations.py
@@ -1,90 +1,80 @@
 import cv2
 import mediapipe as mp
 import pandas as pd
+from variables.joints_ids_to_names import joints_to_track
+from variables.constants import FRAME, JOINT_ID, X_POS, Y_POS, VISIBILITY
 
-mp_pose = mp.solutions.pose
+def video_to_csv(path: str,csv: str):
+    cap = cv2.VideoCapture(path)
+    mp_pose = mp.solutions.pose
 
-# Insertar la dirección del video a analizar 
-video_path = "../postura-project/video/Bien_Caminata_Con_Peso.mp4"
-cap = cv2.VideoCapture(video_path)
+    # Verificar si el video se pudo abrir
+    if not cap.isOpened():
+        print("Error: No se pudo abrir el video.")
+        exit()
 
-# Verificar si el video se pudo abrir
-if not cap.isOpened():
-    print("Error: No se pudo abrir el video.")
-    exit()
+    # Obtener la resolución del video
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    resolution = (width, height)
 
-# Obtener la resolución del video
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-resolution = (width, height)
+    # Calcular los fotogramas por segundo (FPS)
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
-# Calcular los fotogramas por segundo (FPS)
-fps = cap.get(cv2.CAP_PROP_FPS)
+    data = []
 
-data = []
+    pose = mp_pose.Pose(
+        static_image_mode=False,
+        min_tracking_confidence=0.5
+    )    
 
-pose = mp_pose.Pose(
-    static_image_mode=False,
-    min_tracking_confidence=0.5
-)
+    frame_number = 0
 
-# Articulaciones a trackear y sus números
-joints_to_track = {
-    28: "right_ankle",
-    27: "left_ankle",
-    24: "right_hip",
-    23: "left_hip",
-    16:  "right_wrist",
-    15: "left_wrist",
-    12: "right_shoulder",
-    11: "left_shoulder"
-}
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-frame_number = 0
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = pose.process(frame_rgb)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+        if results.pose_landmarks:
+            for joint_id in joints_to_track.items():
+                joint = results.pose_landmarks.landmark[joint_id[0]]
+                
+                # Guardar los datos de las articulaciones con posiciones normalizadas
+                data.append({
+                    FRAME: frame_number,      # Número de frame
+                    JOINT_ID: joint_id,       # Número de la articulación
+                    X_POS: joint.x,           # Posición x (normalizada)
+                    Y_POS: joint.y,           # Posición y (normalizada)
+                    VISIBILITY: joint.visibility  # Visibilidad
+                })
 
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = pose.process(frame_rgb)
+        frame_number += 1
 
-    if results.pose_landmarks:
-        for joint_id, joint_name in joints_to_track.items():
-            joint = results.pose_landmarks.landmark[joint_id]
-            
-            # Guardar los datos de las articulaciones con posiciones normalizadas
-            data.append({
-                'f': frame_number,     # Número de frame
-                'a': joint_id,         # Número de la articulación
-                'x': joint.x,          # Posición x (normalizada)
-                'y': joint.y,          # Posición y (normalizada)
-                'v': joint.visibility  # Visibilidad
-            })
+    cap.release()
 
-    frame_number += 1
+    # Crear DataFrame con los datos recolectados
+    df = pd.DataFrame(data)
 
-cap.release()
+    # Multiplicar los valores de 'x' por el ancho y los de 'y' por el alto para desnormalizar
+    df['x_abs'] = df[X_POS] * resolution[0]
+    df['y_abs'] = df[Y_POS] * resolution[1]
 
-# Crear DataFrame con los datos recolectados
-df = pd.DataFrame(data)
+    # Crear una nueva columna que indica el segundo en que se encuentra cada frame
+    df['second'] = df[FRAME] // fps
 
-# Multiplicar los valores de 'x' por el ancho y los de 'y' por el alto para desnormalizar
-df['x_abs'] = df['x'] * resolution[0]
-df['y_abs'] = df['y'] * resolution[1]
+    # Agrupar por cada segundo y calcular la media de las posiciones x e y para cada articulación
+    grouped_df = df.groupby(['second', JOINT_ID]).agg({
+        'x_abs': 'mean',
+        'y_abs': 'mean',
+        VISIBILITY: 'mean'  # También se puede calcular la visibilidad media
+    }).reset_index()
 
-# Crear una nueva columna que indica el segundo en que se encuentra cada frame
-df['second'] = df['f'] // fps
+    # Guardar el resultado en un nuevo archivo CSV
+    grouped_df.to_csv(csv, index=False)
 
-# Agrupar por cada segundo y calcular la media de las posiciones x e y para cada articulación
-grouped_df = df.groupby(['second', 'a']).agg({
-    'x_abs': 'mean',
-    'y_abs': 'mean',
-    'v': 'mean'  # También se puede calcular la visibilidad media
-}).reset_index()
+    print("Archivo 'joints_per_second.csv' generado correctamente.")
 
-# Guardar el resultado en un nuevo archivo CSV
-grouped_df.to_csv('joints_per_second.csv', index=False)
-
-print("Archivo 'joints_per_second.csv' generado correctamente.")
+video_to_csv("../postura-project/video/Bien_Caminata_Con_Peso.mp4","joints_per_second.csv")
---
+++ variables/constants.py
@@ -0,0 +1,5 @@
+FRAME = 'f'     # Número de frame
+JOINT_ID = 'a'  # Número de la articulación
+X_POS = 'x'     # Posición x (normalizada)
+Y_POS = 'y'     # Posición y (normalizada)
+VISIBILITY = 'v' # Visibilidad

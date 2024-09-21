8a2efcd705b0413f78168fb1cfa1a4af7ba1ab63
Ignacio Dotta <ij.dotta@gmail.com>
2024-09-21 13:54:30 -0300

    add asserts

--- kinematic/kinematic_equations_utils.py
+++ kinematic/kinematic_equations_utils.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 def resultant(x: np.ndarray, y: np.ndarray):
+    assert x.shape == y.shape
     return np.sqrt(np.square(x) + np.square(y))
 
 def direction(x: np.ndarray, y: np.ndarray):
+    assert x.shape == y.shape
     """Returns the angle in radians"""
     return np.arctan(y, x)
 

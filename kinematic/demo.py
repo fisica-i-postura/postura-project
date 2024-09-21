09c3a0614f0da278c07560629dc7cf0b20d4ccc8
Ignacio Dotta <ij.dotta@gmail.com>
2024-09-21 13:36:26 -0300

    fix demo path for pycharm

--- kinematic/demo.py
+++ kinematic/demo.py
@@ -3,6 +3,6 @@ from joints_to_kinematics_data import JointsToKinematicsData
 
 """TODO: extract to main file when integrating changes"""
 
-PATH = './resources/joints_per_second.csv'
+PATH = '../resources/joints_per_second.csv'
 df = pd.read_csv(PATH)
 print(JointsToKinematicsData(df).__str__())

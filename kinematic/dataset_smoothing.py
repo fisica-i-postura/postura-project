9ddbac31b8976340c4c019ef24a1fc76dd7bd145
Ignacio Dotta <ij.dotta@gmail.com>
2024-09-21 14:10:21 -0300

    add docs

--- kinematic/dataset_smoothing.py
+++ kinematic/dataset_smoothing.py
@@ -2,4 +2,5 @@ import numpy as np
 from scipy.signal import savgol_filter
 
 def smooth(dataset: np.ndarray):
+    """Smooths the dataset using Savitzky-Golay filter"""
     return savgol_filter(dataset, window_length=11, polyorder=3)

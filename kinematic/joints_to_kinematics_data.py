0bbcd51fac787b9920ccca450edc4e2747dd3750
Ignacio Dotta <ij.dotta@gmail.com>
2024-09-22 19:30:54 -0300

    add video name to JointToKinematicsData

--- kinematic/joints_to_kinematics_data.py
+++ kinematic/joints_to_kinematics_data.py
@@ -3,8 +3,9 @@ from kinematic.joint_kinematics import JointKinematics
 from variables.constants import JOINT_ID
 
 class JointsToKinematicsData:
-    def __init__(self, df: pd.DataFrame) -> None:
+    def __init__(self, df: pd.DataFrame, name) -> None:
         """Given a dataframe with joints data, models a map of joint_id to JointKinematics"""
+        self.name = name
         joints = df.groupby(JOINT_ID)
         self._joint_id_to_kinematics_map = {joint_id: JointKinematics(data) for joint_id, data in joints}
 
@@ -12,7 +13,7 @@ class JointsToKinematicsData:
         return self._joint_id_to_kinematics_map[joint_id]
     
     def __str__(self) -> str:
-        s = 'Joints Data: \n'
+        s = f'Joints Data for {self.name}: \n'
         for joint, kinematics in self._joint_id_to_kinematics_map.items():
             s += f'{joint} -> {kinematics.__str__()} \n'
         return s

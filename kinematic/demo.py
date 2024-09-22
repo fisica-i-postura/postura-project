92fbd83321cf218eb0986a1e5ce2b9d0796bb158
Ignacio <67470951+ijdotta@users.noreply.github.com>
2024-09-22 18:15:52 -0300

    Task 5.1: smoothing curves (#4)
    * update gitignore
    
    * add sample pandas data
    
    * initial kinematic eq impl
    
    * fix variation functions
    
    * clean up kinematic
    
    * fix accel kinetic
    
    * landmark to kinematic impl
    
    * update fake data
    
    * update fake data
    
    * impl JointKinematics class
    
    * rename landmar_to_kinematics file
    
    * clean up: remove unused files
    
    * clean up
    
    * refactor kinematic equations
    
    * refactor parse joints df
    
    * update .gitignore
    
    * add csv to resources folder
    
    * extract demo file
    
    * use dict comprenhension
    
    * fix typo
    
    * fix typo
    
    * impl dataset smoothing filter
    
    * add smoothed curves (first all rough, then each smooth)
    
    * update demo to plot rough and smooth curves
    
    * better impl of variation of y over x
    
    * fix demo path for pycharm
    
    * impl dataset smoothing filter
    
    * add smoothed curves (first all rough, then each smooth)
    
    * update demo to plot rough and smooth curves
    
    * clean up joint_kinematics.py
    
    * address warnings
    
    * add docs
    
    * fix direction calc
    
    * add asserts
    
    * smooth all raw curves
    
    * add docs

--- kinematic/demo.py
+++ kinematic/demo.py
@@ -1,8 +1,24 @@
 import pandas as pd
 from joints_to_kinematics_data import JointsToKinematicsData
+from joint_kinematics import JointKinematics
+import matplotlib.pyplot as plt
 
 """TODO: extract to main file when integrating changes"""
 
 PATH = '../resources/joints_per_second.csv'
 df = pd.read_csv(PATH)
-print(JointsToKinematicsData(df).__str__())
+joints_data = JointsToKinematicsData(df)
+print(joints_data.__str__())
+
+joint_kinematics: JointKinematics = joints_data.get_joint(11)
+
+def plot(domain, rough_image, smooth_image):
+    plt.figure()
+    plt.plot(domain, rough_image, "-")
+    plt.plot(domain, smooth_image, "--")
+    plt.show()
+
+plot(joint_kinematics.t, joint_kinematics.x_position, joint_kinematics.x_position_smooth)
+plot(joint_kinematics.t, joint_kinematics.y_position, joint_kinematics.y_position_smooth)
+plot(joint_kinematics.t, joint_kinematics.x_velocity, joint_kinematics.x_velocity_smooth)
+plot(joint_kinematics.t, joint_kinematics.y_velocity, joint_kinematics.y_velocity_smooth)

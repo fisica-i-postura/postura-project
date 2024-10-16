c94ddc85a2134ba41ba86cc30be868c201de32b0
Ignacio Dotta <ij.dotta@gmail.com>
2024-10-15 22:48:04 -0300

    integration to main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
+
+from pendulum.plot import plot_pendulum
 from tracking.track_joints_to_pandas_csv import video_to_csv
 import pandas as pd
 from kinematic.joints_to_kinematics_data import JointsToKinematicsData
 from constants.joints_ids_to_names import joints_to_track
 from plotting.graphics_visualization import plot_joint_kinematics
+from pendulum.model import Pendulum
 
 videos_dir = './resources/videos'
 def get_videos_paths():
@@ -39,9 +42,17 @@ def process_kinematics_plots(joints_kinematics_data: list[JointsToKinematicsData
             kinematics = joints_kinematics.get_joint(joint_id)
             plot_joint_kinematics(joint_id, kinematics, joints_kinematics.name)
 
+
+def process_pendulum():
+    for path in get_csv_paths():
+        pendulum = Pendulum(pd.read_csv(path))
+        plot_pendulum(pendulum, 'resources/plots/' + os.path.basename(path).split(".")[0] + '/pendulum.png')
+
+
 if __name__ == '__main__':
-    videos_paths = get_videos_paths()
-    process_videos(videos_paths)
-    kinematics_data = build_kinematics_data()
-    process_kinematics_plots(kinematics_data)
+    # videos_paths = get_videos_paths()
+    # process_videos(videos_paths)
+    # kinematics_data = build_kinematics_data()
+    # process_kinematics_plots(kinematics_data)
+    process_pendulum()
     print('Proceso finalizado')

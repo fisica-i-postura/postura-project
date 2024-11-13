091a4c3af6655e4343afbe6ecfe3c55d21b9d2ad
Ignacio <67470951+ijdotta@users.noreply.github.com>
2024-11-13 19:05:53 -0300

    Revert "Interfaz de usuario (#10)" (#11)
    This reverts commit d07c7c646ca561c3542632493731352bf264b485.

--- constants/joints_ids_to_names.py
+++ constants/joints_ids_to_names.py
@@ -6,8 +6,7 @@ joints_to_track = {
     16: "right_wrist",
     15: "left_wrist",
     12: "right_shoulder",
-    14: "right_elbow",
     11: "left_shoulder",
     14: "right_elbow",
-    16: "right_wrist",
+    26: "right_knee",
 }

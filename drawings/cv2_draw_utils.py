8d51b44f535ebce69e059d9e4830b9798a5a9cc9
Ignacio Dotta <ij.dotta@gmail.com>
2024-11-20 11:35:23 -0300

    fix nan case

--- drawings/cv2_draw_utils.py
+++ drawings/cv2_draw_utils.py
@@ -1,3 +1,5 @@
+from math import isnan
+
 import cv2
 import numpy as np
 
@@ -5,13 +7,17 @@ from drawings.vectors import Vector
 
 Shape = Vector|None
 
-def to_cv2_point(point: tuple[int, int], video_height):
+def to_cv2_point(point: tuple[int, int], video_height) -> tuple[int, int]:
     x, y = point
-    return x, video_height - y
+    if isnan(x) or isnan(y):
+        return 0, 0
+    return int(x), int(video_height - y)
 
 
 def draw_vector(frame: np.ndarray, vector: Vector, video_height):
-    cv2.arrowedLine(frame, to_cv2_point(vector.origin, video_height), to_cv2_point(vector.translation, video_height), (0, 0, 255), 2)
+    pt1 = to_cv2_point(vector.origin, video_height)
+    pt2 = to_cv2_point(vector.translation, video_height)
+    cv2.arrowedLine(frame, pt1, pt2, (0, 0, 255), 2)
 
 
 def draw_shape(frame: np.ndarray, shape: Shape|list[Shape], video_height: int = 1080):

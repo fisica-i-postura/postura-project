7901b89ccb62f71e8248c4aa006b69197cf15204
Ignacio Dotta <ij.dotta@gmail.com>
2024-11-23 18:21:48 -0300

    export with better resolution

--- plotting/kinematics_plots.py
+++ plotting/kinematics_plots.py
@@ -6,6 +6,10 @@ from kinematic.joint_kinematics import JointKinematics
 from plotting.plots import plot_helper
 
 
+def export_fig_as_png(fig, path):
+    write_image(fig, path, format="png", scale=4, width=1280, height=720)
+
+
 class KinematicsPlotHelper:
     def __init__(self, kinematics_data: JointKinematics, joint_name: str, path: Path) -> None:
         self.kinematics_data = kinematics_data
@@ -22,12 +26,12 @@ class KinematicsPlotHelper:
 
         ys = [self.kinematics_data.x_position, self.kinematics_data.y_position]
         fig = plot_helper(self.kinematics_data.t, ys, names, f"{self.joint_name}: Position vs Time", "Time (s)", "Position (m)", "lines")
-        write_image(fig, f"{self.path}/{self.joint_name}_position.png")
+        export_fig_as_png(fig, f"{self.path}/{self.joint_name}_position.png")
         write_html(fig, f"{self.path}/{self.joint_name}_position.html")
 
         ys_smooth = [self.kinematics_data.x_position_smooth, self.kinematics_data.y_position_smooth]
         fig = plot_helper(self.kinematics_data.t, ys_smooth, names, f"{self.joint_name}: Position (smooth) vs Time", "Time (s)", "Position (m)", "lines")
-        write_image(fig, f"{self.path}/{self.joint_name}_position_smooth.png")
+        export_fig_as_png(fig, f"{self.path}/{self.joint_name}_position_smooth.png")
         write_html(fig, f"{self.path}/{self.joint_name}_position_smooth.html")
 
     def plot_velocity(self):
@@ -35,12 +39,12 @@ class KinematicsPlotHelper:
 
         ys = [self.kinematics_data.velocity, self.kinematics_data.x_velocity, self.kinematics_data.y_velocity]
         fig = plot_helper(self.kinematics_data.t, ys, names, f"{self.joint_name}: Velocity vs Time", "Time (s)", "Velocity (m/s)", "lines")
-        write_image(fig, f"{self.path}/{self.joint_name}_velocity.png")
+        export_fig_as_png(fig, f"{self.path}/{self.joint_name}_velocity.png")
         write_html(fig, f"{self.path}/{self.joint_name}_velocity.html")
 
         ys_smooth = [self.kinematics_data.velocity_smooth, self.kinematics_data.x_velocity_smooth, self.kinematics_data.y_velocity_smooth]
         fig = plot_helper(self.kinematics_data.t, ys_smooth, names, f"{self.joint_name}: Velocity (smooth) vs Time", "Time (s)", "Velocity (m/s)", "lines")
-        write_image(fig, f"{self.path}/{self.joint_name}_velocity_smooth.png")
+        export_fig_as_png(fig, f"{self.path}/{self.joint_name}_velocity_smooth.png")
         write_html(fig, f"{self.path}/{self.joint_name}_velocity_smooth.html")
 
     def plot_acceleration(self):
@@ -48,10 +52,10 @@ class KinematicsPlotHelper:
 
         ys = [self.kinematics_data.accel, self.kinematics_data.x_accel, self.kinematics_data.y_accel]
         fig = plot_helper(self.kinematics_data.t, ys, names, f"{self.joint_name}: Acceleration vs Time", "Time (s)", "Acceleration (m/s^2)", "lines")
-        write_image(fig, f"{self.path}/{self.joint_name}_acceleration.png")
+        export_fig_as_png(fig, f"{self.path}/{self.joint_name}_acceleration.png")
         write_html(fig, f"{self.path}/{self.joint_name}_acceleration.html")
 
         ys_smooth = [self.kinematics_data.accel_smooth, self.kinematics_data.x_accel_smooth, self.kinematics_data.y_accel_smooth]
         fig = plot_helper(self.kinematics_data.t, ys_smooth, names, f"{self.joint_name}: Acceleration (smooth) vs Time", "Time (s)", "Acceleration (m/s^2)", "lines")
-        write_image(fig, f"{self.path}/{self.joint_name}_acceleration_smooth.png")
+        export_fig_as_png(fig, f"{self.path}/{self.joint_name}_acceleration_smooth.png")
         write_html(fig, f"{self.path}/{self.joint_name}_acceleration_smooth.html")

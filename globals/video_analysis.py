fffefe4227442e21c8194a145fc227282f946f74
Ignacio Dotta <ij.dotta@gmail.com>
2024-11-23 21:10:03 -0300

    find steps

--- globals/video_analysis.py
+++ globals/video_analysis.py
@@ -1,10 +1,12 @@
+import numpy as np
 import pandas as pd
 
 from constants.df_columns_names import JOINT_ID
-from constants.joints_ids_to_names import get_joint_name
+from constants.joints_ids_to_names import get_joint_name, Joint
 from drawings.vectors import KinematicsVectors
 from globals.video_metadata import VideoMetadata
 from kinematic.joint_kinematics import JointKinematics
+from kinematic.steps import StepsCalculator
 
 
 class JointAnalysis:
@@ -19,10 +21,12 @@ class VideoAnalysis:
         self.video_metadata = video_metadata
         self.joints_raw_data = joints_raw_data
         self.joints_analysis: dict[int, JointAnalysis] = {}
+        self.steps = None
         self.analyse()
 
     def analyse(self):
         self.build_joints_data()
+        self.build_steps()
         """
         TODO: Implement the analysis of the video
             process videos to pandas
@@ -36,3 +40,6 @@ class VideoAnalysis:
     def build_joints_data(self):
         joints_raw_grouped = self.joints_raw_data.groupby(JOINT_ID)
         self.joints_analysis = {joint_id: JointAnalysis(self.video_metadata, joint_data, get_joint_name(joint_id)) for joint_id, joint_data in joints_raw_grouped}
+
+    def build_steps(self):
+        self.steps = StepsCalculator(self.joints_analysis[Joint.RIGHT_HEEL.value].kinematics_data.y_position_smooth).find_steps()

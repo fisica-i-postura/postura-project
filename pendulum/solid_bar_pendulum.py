41f56be826019bd9950e822966e5fe143f752303
Ignacio Dotta <ij.dotta@gmail.com>
2024-11-27 11:07:04 -0300

    fix pendulum angle

--- pendulum/solid_bar_pendulum.py
+++ pendulum/solid_bar_pendulum.py
@@ -7,7 +7,7 @@ from kinematic.joint_kinematics import JointKinematics
 def get_angle_np(fixed_points: np.ndarray, moving_points: np.ndarray) -> np.ndarray:
     xs_diff = moving_points[0] - fixed_points[0]
     ys_diff = moving_points[1] - fixed_points[1]
-    return np.arctan2(xs_diff, ys_diff)
+    return np.arctan2(xs_diff, -ys_diff)
 
 
 class SolidBarPendulumDeprecated:
@@ -31,8 +31,7 @@ class SolidBarPendulum:
         self.mass = mass
         pivot_xy = np.array([pivot.x_position_smooth, pivot.y_position_smooth])
         center_of_mass_xy = np.array([center_of_mass.x_position_smooth, center_of_mass.y_position_smooth])
-        solid_bar_end_xy = np.array([solid_bar_end.x_position_smooth, solid_bar_end.y_position_smooth])
-        self.angle = get_angle_np(pivot_xy, solid_bar_end_xy)
+        self.angle = get_angle_np(pivot_xy, center_of_mass_xy)
         self.angular_velocity = np.full(len(pivot.t), np.nan)
         self.angular_velocity[1:] = np.diff(self.angle) / np.diff(pivot.t)
         self.center_of_mass_to_pivot_distance = np.linalg.norm(pivot_xy - center_of_mass_xy, axis=0)

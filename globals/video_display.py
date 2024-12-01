01649c9ef772857dbb64ad8b64346f35b4235ab9
Ignacio Dotta <ij.dotta@gmail.com>
2024-12-01 19:02:12 -0300

    export video

--- globals/video_display.py
+++ globals/video_display.py
@@ -1,8 +1,11 @@
+from pathlib import Path
+
 import cv2
 
 from constants.joints_ids_to_names import Joint
 from drawings.draw_helper import DrawHelper
 from drawings.draw_configs import DrawAxis, DrawType, JointDrawConfig
+from globals.io.paths import PathHelper
 from globals.video_analysis import VideoAnalysis
 from drawings.colors import Color
 
@@ -24,7 +27,16 @@ def get_draw_configs() -> list[JointDrawConfig]:
 def display(video_path: str, video_analysis: VideoAnalysis) -> None:
     cap = cv2.VideoCapture(video_path)
 
-    # Verificar si el video se pudo abrir
+    # Get video properties
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
+    # Initialize VideoWriter
+    out_path = PathHelper(Path(video_path)).get_processed_video_path().__str__()
+    out = cv2.VideoWriter(out_path, fourcc, fps, (frame_width, frame_height))
+
     if not cap.isOpened():
         print("Error: No se pudo abrir el video.")
         exit()
@@ -39,11 +51,13 @@ def display(video_path: str, video_analysis: VideoAnalysis) -> None:
 
         draw_helper.draw(frame, frame_idx)
         # draw_helper.draw_pendulum_angle(frame, frame_idx)
-        cv2.imshow('Video', frame)
+        out.write(frame)
+        # cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
         frame_idx += 1
 
     cap.release()
+    out.release()
     cv2.destroyAllWindows()

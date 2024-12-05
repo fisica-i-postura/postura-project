6cec082025d50ccd6af88abf3eaa665b20d60d34
Ignacio Dotta <ij.dotta@gmail.com>
2024-12-05 16:45:10 -0300

    use smooth curves

--- kinematic/joint_kinematics.py
+++ kinematic/joint_kinematics.py
@@ -12,48 +12,16 @@ class JointKinematics:
         self.y_position = joint_df[Y_POSITION_ABSOLUTE].to_numpy()
         self.p = joint_df[VISIBILITY].to_numpy()
 
-        self.position = resultant(self.x_position, self.y_position)
-        self.x_velocity = None
-        self.y_velocity = None
-        self.velocity = None
-        self.x_accel = None
-        self.y_accel = None
-        self.accel = None
-        self.x_position_smooth = None
-        self.y_position_smooth = None
-        self.x_velocity_smooth = None
-        self.y_velocity_smooth = None
-        self.velocity_smooth = None
-        self.x_accel_smooth = None
-        self.y_accel_smooth = None
-        self.accel_smooth = None
-
-        self.init_velocity()
-        self.init_accel()
-        self.smooth_curves()
-
-    def init_accel(self):
-        self.x_accel = variation(self.t, self.x_velocity)
-        self.y_accel = variation(self.t, self.y_velocity)
-        self.accel = resultant(self.x_velocity, self.y_velocity)
-
-    def init_velocity(self):
-        self.x_velocity = variation(self.t, self.x_position)
-        self.y_velocity = variation(self.t, self.y_position)
-        self.velocity = resultant(self.x_velocity, self.y_velocity) # TODO: may create tuples (velocity, direction) to plot the vectors
-
-    def smooth_curves(self):
-        """Smooths the curves using Savitzky-Golay filter. ALL curves are first calculated using raw data, then smoothed"""
         self.x_position_smooth = smooth(self.x_position)
         self.y_position_smooth = smooth(self.y_position)
+        self.position = resultant(self.x_position, self.y_position)
 
-        self.x_velocity_smooth = smooth(self.x_velocity)
-        self.y_velocity_smooth = smooth(self.y_velocity)
-        self.velocity_smooth = smooth(self.velocity)
-
-        self.x_accel_smooth = smooth(self.x_accel)
-        self.y_accel_smooth = smooth(self.y_accel)
-        self.accel_smooth = smooth(self.accel)
+        self.x_velocity = self.x_velocity_smooth = variation(self.t, self.x_position_smooth)
+        self.y_velocity = self.y_velocity_smooth = variation(self.t, self.y_position_smooth)
+        self.velocity = self.velocity_smooth = resultant(self.x_velocity, self.y_velocity)
+        self.x_accel = self.x_accel_smooth = variation(self.t, self.x_velocity)
+        self.y_accel = self.y_accel_smooth = variation(self.t, self.y_velocity)
+        self.accel = self.accel_smooth = resultant(self.x_accel, self.y_accel)
     
     def __str__(self) -> str:
         """like java toString(), but for debugging purposes only"""
